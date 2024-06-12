d94a641a3e10524879b79fd3118332c0f3ec9169
Scott Toews <scott.toews@gov.bc.ca>
2024-06-12 11:24:40 -0700

    add controller

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export * from './controller/controller.factory';
 export * from './entities/BaseEntity.class';
 export * from './repository/repository.factory';
 export * from './service/service.factory';
