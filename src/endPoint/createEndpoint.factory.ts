86a1a4462074b995445fa5c933b4ff815240c07b
Scott Toews <scott.toews@gov.bc.ca>
2024-06-13 11:02:36 -0700

    remove excess comments

--- src/endPoint/createEndpoint.factory.ts
+++ src/endPoint/createEndpoint.factory.ts
@@ -1,16 +1,9 @@
-// import { Controller } from '@/controller';
-// import { Service } from '@/service';
 import { Endpoint } from './endpoint.class';
-// import { Repository } from '@/repository';
 import { DataSource } from 'typeorm';
 
 export interface createEndpointProps<TEntity> {
-  // dataRepository: Repository<TEntity>;
   entity: TEntity;
-  // dataService: Service<TEntity>;
-  // dataController: Controller<TEntity>;
   dataSource: DataSource;
-  // entities?: EntitySchema[];
 }
 
 export const createEndpoints = <TEntity>(props: createEndpointProps<TEntity>) => {
