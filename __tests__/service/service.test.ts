96907aa234b52c7766764b51a0c6f2856317d75e
Scott Toews <scott.toews@gov.bc.ca>
2024-06-20 14:16:40 -0700

    added service tests

---
+++ __tests__/service/service.test.ts
@@ -0,0 +1,104 @@
+import { Repository, createRepository } from '@/repository';
+import { createService, Service, IFormattedResponse } from '@/service';
+import { mockDataSource, MockEntity, testItem, testItems, testUUID } from '__tests__/__mocks__';
+// import { EntitySchema } from 'typeorm';
+
+describe('Servive', () => {
+  let testRepository: Repository<MockEntity>;
+  let testService: Service<MockEntity>;
+  let formattedResponseAll: IFormattedResponse<MockEntity>;
+  let formattedResponseSingle: IFormattedResponse<MockEntity>;
+  let formattedResponseEmpty: IFormattedResponse<MockEntity>;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    testRepository = createRepository<MockEntity>(new MockEntity(), mockDataSource);
+    testService = createService<MockEntity>(testRepository);
+    formattedResponseAll = { data: testItems };
+    formattedResponseSingle = { data: [testItem] };
+    formattedResponseEmpty = { data: [] };
+  });
+
+  it('should create a service', () => {
+    expect(testService).toBeInstanceOf(Service<MockEntity>);
+    expect(testService).toHaveProperty('getAllItems');
+    expect(testService).toHaveProperty('getItemById');
+    expect(testService).toHaveProperty('getItemByWhere');
+    expect(testService).toHaveProperty('createItem');
+    expect(testService).toHaveProperty('createItems');
+    expect(testService).toHaveProperty('updateItemById');
+    expect(testService).toHaveProperty('deleteItemById');
+  });
+
+  it('should get all items', async () => {
+    const items = await testService.getAllItems();
+
+    expect(items).toEqual(formattedResponseAll);
+  });
+
+  describe('getItemById', () => {
+    it('when exists should return the item', async () => {
+      const item = await testService.getItemById(testUUID);
+
+      expect(item).toStrictEqual(formattedResponseSingle);
+    });
+
+    it('when does not exist should return', async () => {
+      const item = await testService.getItemById('NOTtestUUID');
+
+      expect(item).toStrictEqual(formattedResponseEmpty);
+    });
+  });
+
+  describe('getItemByWhere', () => {
+    it('when exists should return the item', async () => {
+      const item = await testService.getItemByWhere({ id: testUUID });
+
+      expect(item).toStrictEqual(formattedResponseSingle);
+    });
+
+    it('when does not exist should return', async () => {
+      const item = await testService.getItemByWhere({ id: 'NOTtestUUID' });
+
+      expect(item).toStrictEqual(formattedResponseEmpty);
+    });
+  });
+
+  it('should create an item', async () => {
+    const item = await testService.createItem(testItem);
+
+    expect(item).toStrictEqual(formattedResponseSingle);
+  });
+
+  it('should create multiple items', async () => {
+    const items = await testService.createItems(testItems);
+
+    expect(items).toStrictEqual(formattedResponseAll);
+  });
+
+  describe('update an item by id', () => {
+    it('when item exists should return updated item', async () => {
+      const itemToUpdate = { ...testItem, title: 'updated' };
+      const updatedItem = await testService.updateItemById(testUUID, itemToUpdate);
+
+      const formattedUpdatedItem = { data: [itemToUpdate] };
+
+      expect(updatedItem).toStrictEqual(formattedUpdatedItem);
+    });
+
+    it('when item does not exist should return empty data', async () => {
+      const updatedItem = await testService.updateItemById('NOTtestUUID', {
+        ...testItem,
+        title: 'updated',
+      });
+
+      expect(updatedItem).toStrictEqual(formattedResponseEmpty);
+    });
+  });
+
+  it('should delete an item by id', async () => {
+    const result = await testService.deleteItemById(testUUID);
+
+    expect(result).toStrictEqual(formattedResponseEmpty);
+  });
+});
--- src/service/service.class.ts
+++ src/service/service.class.ts
@@ -1,69 +1,73 @@
-import { EntitySchema } from 'typeorm';
+// import { EntitySchema } from 'typeorm';
 import type { Repository } from '../repository/repository.factory';
 
-interface IFormattedResponse<TEntity> {
-  data: TEntity | TEntity[] | undefined;
+export interface IFormattedResponse<TEntity> {
+  data: TEntity[];
 }
 export class Service<TEntity> {
-  private formatResponse = <TEntity>(
-    body: TEntity | TEntity[] | undefined,
-  ): IFormattedResponse<TEntity> => {
-    return {
-      data: body,
-    };
+  protected formatResponse = (data: TEntity | TEntity[] | null): IFormattedResponse<TEntity> => {
+    if (data === null) data = [];
+    if (!Array.isArray(data)) data = [data];
+
+    return { data };
   };
 
-  getAllItems: () => Promise<IFormattedResponse<TEntity>>;
+  public getAllItems: () => Promise<IFormattedResponse<TEntity>>;
 
-  getItemById: (_id: string) => Promise<IFormattedResponse<TEntity>>;
+  public getItemById: (_id: string) => Promise<IFormattedResponse<TEntity>>;
 
-  getItemByWhere: (_where: object) => Promise<IFormattedResponse<TEntity>>;
+  public getItemByWhere: (_where: object) => Promise<IFormattedResponse<TEntity>>;
 
-  createItem: (_item: TEntity) => Promise<IFormattedResponse<TEntity>>;
+  public createItem: (_item: TEntity) => Promise<IFormattedResponse<TEntity>>;
 
-  createItems: (_items: TEntity[]) => Promise<IFormattedResponse<TEntity[]>>;
+  public createItems: (_items: TEntity[]) => Promise<IFormattedResponse<TEntity>>;
 
-  updateItem: (_id: string, _item: TEntity) => Promise<IFormattedResponse<TEntity>>;
+  public updateItemById: (_id: string, _item: TEntity) => Promise<IFormattedResponse<TEntity>>;
 
-  deleteItem: (_id: string) => Promise<IFormattedResponse<TEntity>>;
+  public deleteItemById: (_id: string) => Promise<IFormattedResponse<TEntity>>;
 
-  // eslint-disable-next-line no-unused-vars
-  constructor(private repository: Repository<TEntity>) {
+  constructor(private readonly repository: Repository<TEntity>) {
+    this.repository = repository;
+
     this.getAllItems = async () => {
       const response = await this.repository.getAllItems();
-      return this.formatResponse<TEntity>(response as TEntity[]);
+      return this.formatResponse(response as TEntity[]);
     };
 
     this.getItemById = async (id: string) => {
       const response = await this.repository.getItemById(id);
-      return this.formatResponse<TEntity>(response as TEntity);
+      return this.formatResponse(response as TEntity);
     };
 
     this.getItemByWhere = async (where: object) => {
       const response = await this.repository.getItemByWhere(where);
-      return this.formatResponse<TEntity>(response as TEntity);
+      return this.formatResponse(response as TEntity);
     };
 
     this.createItem = async (item: TEntity) => {
-      const response = this.repository.createItem(item as unknown as EntitySchema<TEntity>);
-      return this.formatResponse<TEntity>(response as TEntity);
+      const response = await this.repository.createItem(item);
+      return this.formatResponse(response as TEntity);
     };
 
     this.createItems = async (items: TEntity[]) => {
-      const responses = items.map((item) =>
-        this.repository.createItem(item as unknown as EntitySchema<TEntity>),
-      );
-      return this.formatResponse<TEntity[]>(responses as TEntity[]);
+      const responses: TEntity[] = [];
+
+      for (let id = 0; id < items.length; id++) {
+        const response = await this.repository.createItem(items[id]);
+        responses.push(response as TEntity);
+      }
+
+      return this.formatResponse(responses);
     };
 
-    this.updateItem = async (id: string, item: TEntity) => {
-      const response = this.repository.updateItemById(id, item as unknown as EntitySchema<TEntity>);
-      return this.formatResponse<TEntity>(response as TEntity);
+    this.updateItemById = async (id: string, item: TEntity) => {
+      const response = await this.repository.updateItemById(id, item);
+      return this.formatResponse(response as TEntity);
     };
 
-    this.deleteItem = async (id: string) => {
-      const response = this.repository.deleteItemById(id);
-      return this.formatResponse<TEntity>(response as TEntity);
+    this.deleteItemById = async (id: string) => {
+      const response = await this.repository.deleteItemById(id);
+      return this.formatResponse(response as TEntity);
     };
   }
 }
